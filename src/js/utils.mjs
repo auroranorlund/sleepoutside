35f2f53db690714b1a1646b3ff1c6c436db131df
Aurora Norlund <auroranorlund@gmail.com>
2025-11-07 21:35:52 -0700

    product details page fix

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -25,7 +25,8 @@ export function setClick(selector, callback) {
 export function getParam(param) {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
-  const product = urlParams.get('product')
+  const product = urlParams.get(param);
+  return product
 }
 // render HTML template
 export function renderListWithTemplate(templateFn, parentElement, list, position = "afterbegin", clear = false) {
