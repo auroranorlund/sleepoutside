dde40f991ba5a3526e48c16b69c90741fe6a4075
Aurora Norlund <auroranorlund@gmail.com>
2025-11-11 20:53:00 -0700

    team activity 3

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -49,3 +49,13 @@ export async function loadTemplate(path) {
   const template = await res.text();
   return template;
 }
+// load header and footer
+export async function loadHeaderFooter() {
+  const headerTemplate = await loadTemplate("../partials/header.html");
+  const headerElement = document.querySelector("#main-header");
+  renderWithTemplate(headerTemplate, headerElement);
+
+  const footerTemplate = await loadTemplate("../partials/footer.html");
+  const footerElement = document.querySelector("#main-footer");
+  renderWithTemplate(footerTemplate, footerElement);
+}
