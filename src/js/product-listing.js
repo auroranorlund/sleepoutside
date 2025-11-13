b6fe335ad8a20a2ed8388b9c8c5c4440299e390a
Aurora Norlund <auroranorlund@gmail.com>
2025-11-13 13:45:19 -0700

    an-individual3

---
+++ src/js/product-listing.js
@@ -0,0 +1,23 @@
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
+import { loadHeaderFooter, getParam } from "./utils.mjs";
+
+loadHeaderFooter();
+
+const category = getParam('category');
+
+document.getElementById('listing-title').innerHTML = `Top Products: ${category.charAt(0).toUpperCase() + category.slice(1)}`;
+
+if (category == "sleeping-bags") {
+  document.getElementById('listing-title').innerHTML = `Top Products: Sleeping Bags`;
+}
+
+const dataSource = new ProductData();
+
+const list = new ProductList(
+  category,
+  dataSource,
+  document.querySelector(".product-list"),
+);
+
+list.init();
